1aa5e84abac02f9f8af5f5995bfe9055e0965d6d
中村　迅 <shu.kyu0224@icloud.com>
2021-08-21 20:02:39 +0900

    rese

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -70,7 +70,7 @@ export default {
             password: this.Password
           }
 
-          axios.post('https://www.cleardb.com/service/1.0/api/v1/users/register', sendData)
+          axios.post('https://thawing-brook-30782.herokuapp.com/api/v1/users/register', sendData)
           firebase.auth().currentUser.sendEmailVerification()
             .then(() => {
               this.$router.push("/thanks");
