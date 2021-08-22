2db6cfa12d93905fb16b96d47100bae66c320d76
中村　迅 <shu.kyu0224@icloud.com>
2021-08-22 19:20:14 +0900

    rese

--- src/views/Owner/ShopRegister.vue
+++ src/views/Owner/ShopRegister.vue
@@ -192,7 +192,6 @@ export default {
             if(sendShopData.image  == undefined) {
               sendShopData.image = 'no_image.png'
             }
-            console.log(sendShopData)
             axios.post('https://rese-app.herokuapp.com/api/v1/owners/' + user.uid + '/shop', sendShopData)
             .then(() => {
               this.createdShop = true;
