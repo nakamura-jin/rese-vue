d49e61b5b615a967201656e458a8ce4e5e5e5a53
中村　迅 <shu.kyu0224@icloud.com>
2021-08-23 11:58:35 +0900

    rese

--- src/views/Owner/OwnerRegister.vue
+++ src/views/Owner/OwnerRegister.vue
@@ -69,7 +69,7 @@ export default {
             email: this.OwnerEmail,
             password: this.OwnerPassword
           }
-          axios.post('https://rese-app.herokuapp.com/api/v1/owners/register', sendOwnerData);
+          axios.post("https://rese-app.herokuapp.com/api/v1/owners/register", sendOwnerData)
           this.$router.push("/owner/thanks")
         })
         .catch((error) => {
